75e2afeb7104127a712e29520abdb3e2ee944fc2
Alex <oleksiymosiychuk108@gmail.com>
2025-08-14 08:54:04 +0300

    Add setter, getter and new methods in class Product

--- src/product.py
+++ src/product.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class Product:
     """
     Class Product descriptions name, short description, price and quantity of product
@@ -7,6 +10,8 @@ class Product:
     description: str
     price: float
     quantity: int
+
+    products_list: list = []
 
     def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
         """
@@ -20,5 +25,67 @@ class Product:
 
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+        Product.products_list.append(self)
+
+    @classmethod
+    def new_product(cls, product_in_dict: dict) -> Any:
+
+        for product in cls.products_list:
+
+            if product.name == product_in_dict["name"]:
+                product.description = product_in_dict["description"]
+                product.__price = (
+                    product_in_dict["price"] if product_in_dict["price"] > product.__price else product.price
+                )
+                product.quantity += product_in_dict["quantity"]
+
+                return product
+
+        name = product_in_dict["name"]
+        description = product_in_dict["description"]
+        __price = product_in_dict["price"]
+        quantity = product_in_dict["quantity"]
+
+        return cls(name, description, __price, quantity)
+
+    @property
+    def price(self) -> float:
+        return self.__price
+
+    @price.setter
+    def price(self, price: float) -> None:
+        if price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        elif price < self.__price:
+            answer = input("Вы понижаете стоимость товара! Подтвердить?(y/n): ")
+            if answer.lower() == "y":
+                self.__price = price
+        else:
+            self.__price = price
+
+
+# if __name__ == "__main__":
+#     pass
+# prod_1 = Product(name="Lemon", description="Yellow lemon", price=6.40, quantity=600)
+# print(prod_1.name)
+#
+# print(Product.new_product({"name": "Банан", "description": "Yellow lemon", "price": 6.40, "quantity": 60}).name)
+#
+# print(Product.products_list)
+#
+# print(Product.new_product({"name": "Банан", "description": "Yellow lemon", "price": 7.40, "quantity": 60}).price)
+#
+# print(Product.products_list[1].price)
+# Product.products_list[1].price = 7.2
+#
+# print(Product.products_list[1].price)
+#
+# print(Product.new_product({"name": "Банан", "description": "Yellow lemon", "price": 7.40, "quantity": 60}).price)
+#
+# print(Product.products_list)
+# # print(prod_2.name)
+# # print(prod_2.description)
+# # print(prod_2.price)
