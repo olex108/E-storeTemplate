f16438dd712ef9824b370c2f540e52daa93f1c93
Alex <oleksiymosiychuk108@gmail.com>
2025-08-31 00:24:58 +0300

    add new class order.py

---
+++ src/order.py
@@ -0,0 +1,45 @@
+from src.product import Product
+from src.abstract_classes import BaseOrder
+
+
+class Order(BaseOrder):
+    """
+    The class describes product, quantity and price in order
+    """
+
+    product: Product
+    quantity: int
+
+
+    def __init__(self, product: Product, quantity: int) -> None:
+        """
+        Initialization method
+        :param product: Product instance
+        :param quantity: Quantity
+        total_price: Total price
+
+        """
+
+        self.product = product
+        self.quantity = self.check_quantity(quantity)
+        self.total_price = round(product.price * self.quantity, 2)
+
+
+    def __str__(self):
+        return f"{self.product.name}, quantity: {self.quantity}, total price: {self.total_price}"
+
+
+    def check_quantity(self, quantity) -> int:
+        if quantity <= self.product.quantity:
+            self.product.quantity -= quantity
+            return quantity
+        else:
+            print(f"Max quantity of is {self.product.quantity}")
+            max_quantity = self.product.quantity
+            self.product.quantity = 0
+            return max_quantity
+
+
+    @property
+    def products(self) -> str:
+        return f"{self.product.name}, quantity: {self.quantity}, total price: {self.total_price}"
